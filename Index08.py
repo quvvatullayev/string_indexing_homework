1f6f976924607c7a91265e8b6b97e885e7c092ec
tajriba <quvvatullayev@gmail.com>
2022-06-28 18:41:04 +0300

    Index08.py

--- Index08.py
+++ Index08.py
@@ -1,3 +1,6 @@
+from cgi import print_environ
+
+
 def main(s):
     """
     A string of length five is given. Return the index of the "*" character, return False if not present.
@@ -6,5 +9,12 @@ def main(s):
     Returns:
         int: answer
     """
-    return
-        
+    s = str(s)
+    if int(s.index("*")) >= 0:
+        return s.index("*")
+
+    else:
+        return False
+
+
+print(main("Ogabe*"))
