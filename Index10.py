bc019f6ef5db564f5aa2ee20dbee2c3fd030b2d7
tajriba <quvvatullayev@gmail.com>
2022-06-28 18:50:37 +0300

    Index10.py

--- Index10.py
+++ Index10.py
@@ -6,4 +6,22 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    s = int(s)
+    s1 = s%10
+    s //= 10
+
+    s2 = s%10
+    s //= 10
+
+    s3 = s%10
+    s //= 10
+
+    s4 = s%10
+    s //= 10
+
+    s5 = s%10
+    s //= 10
+
+    return s1+s2+s3+s4+s5
+
+print(main("12345"))
